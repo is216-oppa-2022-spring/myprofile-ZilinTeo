61c79c8f2b40e7a2bb7785d9a84ad18c60426125
Zi Lin Teo <zilin.teo.2019@scis.smu.edu.sg>
2022-02-22 02:43:48 +0800

    Updated form.html

--- js/form.js
+++ js/form.js
@@ -1,6 +1,9 @@
 function submit(){
     var form = document.getElementById('form-box')
-    console.log(form)
+    var name = document.getElementById('name')
+    var message = document.getElementById('message')
+    console.log(name)
+    console.log(message)
     form.style.display = "none"
 
 }
